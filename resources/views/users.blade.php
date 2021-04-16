c28f42674fc22ce7c557a0f1ff7210c6c0409bd1
kekaswork <kekaswork@gmail.com>
2021-04-16 17:29:43 +0300

    testing commit

---
+++ resources/views/users.blade.php
@@ -0,0 +1,16 @@
+
+<table>
+	<caption>Users</caption>
+	<tr>
+		<th>ID</th>
+		<th>Name</th>
+		<th>Surname</th>
+		<th>Some Data</th>
+	</tr>
+	<tr>
+		<td>1</td>
+		<td>Almaz</td>
+		<td>Galimyanov</td>
+		<td>foo bar</td>
+	</tr>	
+</table>
