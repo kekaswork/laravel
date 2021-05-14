ef41173a004a3ca5f233bc444aef9215956a0a98
kekaswork <kekaswork@gmail.com>
2021-05-14 23:06:39 +0300

    Fixes after feedback

--- routes/api.php
+++ routes/api.php
@@ -19,12 +19,6 @@ use App\Http\Controllers\UserController;
 |
 */
 
-// By the way, thank you for your attention to me as a candidate,
-// despite my terrible first attempt you gave me a chance to improve my code, I appreciate it.
-// Unfortunately, I have never built Laravel's API, so it was my first experience and even if you will not proceed
-// recruitment process with me, I found committing this test task very useful for my professional career, since I learnt a lot of new staff.
-// Have a nice day!
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
